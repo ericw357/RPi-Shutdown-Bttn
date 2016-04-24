4e0016661b702fa14bb33525d650435a37cbe141
ericw357 <ericw357@gmail.com>
2016-04-24 09:23:28 -0700

    Create pishutdown.py

---
+++ pishutdown.py
@@ -0,0 +1,17 @@
+Enter file conte#!/usr/bin/python
+
+# Import the modules to send commands to the system and access GPIO pins
+import RPi.GPIO as gpio
+import os
+
+#Set pin numbering to board numbering
+gpio.setmode(gpio.BOARD)
+
+#Set up pin 7 as an input
+gpio.setup(7, gpio.IN) 
+
+# Set up an interrupt to look for pressed button
+gpio.wait_for_edge(7, gpio.FALLING) 
+
+# Shutdown
+os.system('shutdown now -h')nts here
