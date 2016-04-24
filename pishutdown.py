34186f290b9c6ad5379c09d6ccbc96021cd53094
ericw357 <ericw357@gmail.com>
2016-04-24 10:10:42 -0700

    Update pishutdown.py

--- pishutdown.py
+++ pishutdown.py
@@ -9,12 +9,15 @@ gpio.setmode(gpio.BCM)
 
 #Set up pin 4 as an input
 gpio.setup(4, gpio.IN, pull_up_down = gpio.PUD_UP) 
+#Set up pin 17 as an input
+gpio.setup(17, gpio.IN, pull_up_down = gpio.PUD_UP) 
 
 # Set up an interrupt to look for pressed button
 gpio.wait_for_edge(4, gpio.FALLING) 
-
-# Shutdown
-# os.system('shutdown now -h')nts here
-
 # Reboot
 os.system('reboot')
+
+# Set up an interrupt to look for pressed button
+gpio.wait_for_edge(17, gpio.FALLING) 
+# Shutdown
+os.system('shutdown now -h')
