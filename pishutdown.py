cd965cf2db4303f6e23a2d7a2540efd88a6061db
ericw357 <ericw357@gmail.com>
2016-04-24 18:09:38 -0700

    Update pishutdown.py

--- pishutdown.py
+++ pishutdown.py
@@ -10,14 +10,18 @@ gpio.setmode(gpio.BCM)
 #Set up pin 4 as an input
 gpio.setup(4, gpio.IN, pull_up_down = gpio.PUD_UP) 
 #Set up pin 17 as an input
-gpio.setup(17, gpio.IN, pull_up_down = gpio.PUD_UP) 
+gpio.setup(17, gpio.IN, pull_up_down = gpio.PUD_UP)
+
+# define a callback function for the reboot
+def my_callback(channel):
+	# Reboot
+	os.system('reboot')
+
+# Set up an interrupt to look for pressed button
+gpio.add_event_detect(17, gpio.FALLING, callback=my_callback, bouncetime=300) 
 
 # Set up an interrupt to look for pressed button
 gpio.wait_for_edge(4, gpio.FALLING) 
-# Reboot
-os.system('reboot')
-
-# Set up an interrupt to look for pressed button
-gpio.wait_for_edge(17, gpio.FALLING) 
 # Shutdown
 os.system('shutdown now -h')
+
